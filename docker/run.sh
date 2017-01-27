2b2f5456029f1ee4996809ebd0faab14305bc1b8
kurian <kurian@hifx.co.in>
2017-01-27 16:47:22 +0530

    reforming the structure
    making code and docker folders. Adding a Dockerfile using the docker
    file to install java in an isolated container.

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,6 @@
 docker rm -f jDev
 docker run --name jDev \
 	-ti \
-	-v $HOME/code/C/code:/home/code \
-	--env-file $HOME/code/C/conf/c.env \
+	-v $HOME/code/JNI/code:/home/code \
+	--env-file $HOME/code/JNI/conf/java.env \
 	 $1 /bin/bash
